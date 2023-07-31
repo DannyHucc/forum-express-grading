9a18966976ee9e11c69ce7687f63f61631b90a44
DannyHucc <dannyhu10011101@gmail.com>
2023-07-31 14:37:48 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -45,6 +45,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
