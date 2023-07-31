6f16818345143c183d91442d58422a262c7e0b1a
DannyHucc <dannyhu10011101@gmail.com>
2023-07-31 15:04:44 +0800

    feat: remove api server

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages, apis } = require('./routes')
+const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -45,8 +45,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use('/api', apis)
-app.use(pages)
+app.use(routes)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
--- routes/apis/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const restController = require('../../controllers/apis/restaurant-controller')
-
-router.get('/restaurants', restController.getRestaurants)
-
-module.exports = router
